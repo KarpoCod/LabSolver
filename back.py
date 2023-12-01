7005c812a63dcf018487a94c74548e65929e4b01
Alek1101 <151769945+Alek1101@users.noreply.github.com>
2023-12-01 22:07:21 +0300

    Big_bug_fix.py

--- back.py
+++ back.py
@@ -7,18 +7,20 @@ def inputf(inp):
         except:
             x = [x[1:]]
         try:
-            out = float(float(str(x[0]) + "." + str(x[1])) if len(x) > 1  else float(x[0]))
+            out = float(float(str(x[0]) + "." + str(x[1])) if len(x) > 1 else float(x[0]))
             fl = False
         except:
             print("вы ввели не число")
-            print(inp, end = '')
+            print(inp, end=' ')
     return out
+
 
 def lab_work_5():
     print('Введите данные таймера:')
     t = []
     for i in range(4):
-        t.append(inputf("t" + str(i) + " = "))
+        print(f't{i + 1}: ')
+        t.append(inputf("t" + str(i+1) + " = "))
 
     l = 10 / 1000
     M = 510 / 1000
@@ -28,12 +30,12 @@ def lab_work_5():
 
     v = []
     for Ti in t:
-        v.append(l/Ti)
+        v.append(l / Ti)
     print('v (скорость): ', "{:.8f}".format(v[0]), "{:.8f}".format(v[1]), "{:.8f}".format(v[2]), "{:.8f}".format(v[3]))
 
     p = []
     for vel in v:
-        p.append(M*vel)
+        p.append(M * vel)
     print('p (импульс): ', "{:.8f}".format(p[0]), "{:.8f}".format(p[1]), "{:.8f}".format(p[2]), "{:.8f}".format(p[3]))
 
     w_t = []
@@ -42,31 +44,31 @@ def lab_work_5():
     print('δt (относительная погрешность времени): ', "{:.8f}".format(w_t[0]), "{:.8f}".format(w_t[1]),
           "{:.8f}".format(w_t[2]), "{:.8f}".format(w_t[3]))
     a = d_l / l + d_M / M
-    w_p1 = a + w_t1
-    w_p2 = a + w_t2
-    w_p3 = a + w_t3
-    w_p4 = a + w_t4
+    w_p1 = a + w_t[0]
+    w_p2 = a + w_t[1]
+    w_p3 = a + w_t[2]
+    w_p4 = a + w_t[3]
     print('δp (относительная погрешность импульса): ', "{:.8f}".format(w_p1), "{:.8f}".format(w_p2),
           "{:.8f}".format(w_p3), "{:.8f}".format(w_p4))
-    d_p1 = w_p1 * p1
-    d_p2 = w_p2 * p2
-    d_p3 = w_p3 * p3
-    d_p4 = w_p4 * p4
+    d_p1 = w_p1 * p[0]
+    d_p2 = w_p2 * p[1]
+    d_p3 = w_p3 * p[2]
+    d_p4 = w_p4 * p[3]
     print('Δp (абсолютная погрешность импульса): ', "{:.8f}".format(d_p1), "{:.8f}".format(d_p2),
           "{:.8f}".format(d_p3), "{:.8f}".format(d_p4))
-    print(f'(p1 = {"{:.8f}".format(p1)} +- {"{:.8f}".format(d_p1)})')
-    print(f'(p2 = {"{:.8f}".format(p2)} +- {"{:.8f}".format(d_p2)})')
-    print(f'(p3 = {"{:.8f}".format(p3)} +- {"{:.8f}".format(d_p3)})')
-    print(f'(p4 = {"{:.8f}".format(p4)} +- {"{:.8f}".format(d_p4)})')
+    print(f'(p1 = {"{:.8f}".format(p[0])} +- {"{:.8f}".format(d_p1)})')
+    print(f'(p2 = {"{:.8f}".format(p[1])} +- {"{:.8f}".format(d_p2)})')
+    print(f'(p3 = {"{:.8f}".format(p[2])} +- {"{:.8f}".format(d_p3)})')
+    print(f'(p4 = {"{:.8f}".format(p[3])} +- {"{:.8f}".format(d_p4)})')
     input('Enter для продолжения (если ничего не происходит, ещё раз Enter)')
 
 
 def lab_work_6():
     print('Вводить: \nF \nd_s (абсолютная погрешность отклонения среднего арифметического s) \nH \nh')
-    F = inputf()
-    d_s = inputf()
-    H = inputf()
-    h = inputf()
+    F = inputf(input())
+    d_s = inputf(input())
+    H = inputf(input())
+    h = inputf(input())
 
     w_m1 = 0.000121
     w_m2 = 0.000776
@@ -83,11 +85,4 @@ def lab_work_6():
     print('w_F:', "{:.8f}".format(w_F))
     print('d_F:', "{:.8f}".format(d_F))
     print(f'F = ({"{:.8f}".format(F)} +- {"{:.8f}".format(d_F)})')
-    input('Enter для продолжения')
-#
-#
-# F = float(input())
-# d_s = float(input())
-# H = float(input())
-# h = float(input())
-# lab_work_6(F, d_s, H, h)
+    input('Enter для продолжения (если ничего не происходит, ещё раз Enter)')
